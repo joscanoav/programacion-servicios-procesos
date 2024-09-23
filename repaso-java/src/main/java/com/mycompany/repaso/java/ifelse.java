df6e26b65f05141f2c7e455e0149d854c914a5a8
joscanoav <joscanoav@gmail.com>
2024-09-23 02:37:54 +0200

    Segundo progra de ifelse

---
+++ repaso-java/src/main/java/com/mycompany/repaso/java/ifelse.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.repaso.java;
+
+/**
+ *
+ * @author josca
+ * OPERADORES LOGICOS
+ * > MAYOR
+ * >= MAYOR IGUAL
+ * < MENOR
+ * <= MENOR IGUAL
+ * == COMPARADOR
+ * && AND
+ * || OR
+ */
+public class ifelse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic 
+        int var;
+        var=200;
+        if (var==3){
+        // si la operacion cumple
+            System.out.println("Soy un tres!!");
+            
+        }
+        else{
+        // si la operacion no cumple
+            System.out.println("No soy un tres!!");
+        }
+    }
+    
+}
