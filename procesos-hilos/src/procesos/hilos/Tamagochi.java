03bc9a599f735e76cf042282ee3ead0014a788e7
joscanoav <joscanoav@gmail.com>
2024-10-23 15:20:29 +0200

    Creacion de Tamagochi REY ESCLAVO

---
+++ procesos-hilos/src/procesos/hilos/Tamagochi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package procesos.hilos;
+
+/**
+ *
+ * @author josca
+ */
+public class Tamagochi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
